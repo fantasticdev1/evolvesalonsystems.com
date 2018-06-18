7f71fbabf80a7a2e0600f5c593c6bab8401af603
Adam Cook <adam@onecreative.pro>
2018-06-18 09:29:49 -0500

    Update Models

--- marketplace_builder/assets/admin/scripts/dtables.js
+++ marketplace_builder/assets/admin/scripts/dtables.js
@@ -9,13 +9,16 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 		version: '1.0.0',
 		date: '2018.06.05',
 		defaults: {
+			resource: null,
 			form: null,
 			parent: null,
 			fields: null,
 			records: null,
 			editClass: 'edit_record',
 			deleteClass: 'delete_record',
-			addClass: 'add_record'
+			addClass: 'add_record',
+			container: 'selector',
+			buttonContainer: null
 		}
 	});
 
@@ -23,21 +26,19 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 
 	};
 
-	if (settings.fields !== null && settings.records !== null && settings.config !== null) {
+	if (settings.resource !== null && settings.fields !== null && settings.records !== null && settings.config !== null) {
 		let fields = fetch(settings.fields).then(response => response.json());
 		let form = fetch(settings.form).then(response => response.json());
+		let container = (settings.container === 'selector') ? selector : selector.closest(settings.container);
 
 		Promise.all([fields,form]).then((response) => {
 			let fields = response[0];
 			let form = response[1];
-			let columns = [];
-			for (let field in form.configuration.properties) columns.push({title:field, name:field, data:field, defaultContent:''});
-			columns.push({title:'Actions', name:'Actions', data:null, defaultContent:'<a href="#" class="btn btn-warning btn-link btn-icon btn-md"><i class="fas fa-edit edit_record"></i></a><a href="#" class="btn btn-danger btn-link btn-icon btn-md"><i class="fas fa-times delete_record"></i></a>'});
 
 			const editor = new $.fn.dataTable.Editor( {
 				ajax:  {
 					create: function(method, url, data, success, error) {
-						posdk.model.item.save({
+						posdk[settings.resource].item.save({
 							form: form.name,
 							parent: settings.parent,
 							fields: data.data[0] || {}
@@ -57,7 +58,7 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 					},
 					edit: function(method, url, data, success, error) {
 						let id = Object.getOwnPropertyNames(data.data)[0];
-						posdk.model.item.save({
+						posdk[settings.resource].item.save({
 							id: id,
 							form: form.name,
 							parent: settings.parent,
@@ -74,7 +75,7 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 					},
 					remove: function(method, url, data, success, error) {
 						let id = Object.getOwnPropertyNames(data.data)[0];
-						posdk.model.item.delete({
+						posdk[settings.resource].item.delete({
 							id: id,
 							form: form.name
 						})
@@ -92,6 +93,16 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 				fields: fields
 			});
 
+			let columns = [{
+				title: '&nbsp;&nbsp;&nbsp;',
+                data: null,
+                defaultContent: '',
+                className: 'select-box',
+                orderable: false,
+                searchable: false
+            }];
+			for (let field in form.configuration.properties) columns.push({title:field, name:field, data:field, defaultContent:''});
+
 			const table = selector.DataTable({
 				ajax: settings.records,
 				columns: columns,
@@ -99,52 +110,78 @@ posdk.extensions.tricks.dtables = function(selector,options) {
 				stateSave: true,
 				rowId: 'id',
 				pagingType: 'full_numbers',
-				// fixedColumns: {
-		  //           leftColumns: 1
-		  //       },
 				lengthMenu: [
 					[10, 25, 50, -1],
 					[10, 25, 50, 'All']
 				],
-				responsive: true,
+				select: {
+					blurable: true,
+					selector: 'td:first-child',
+					style: 'os'
+				},
+				colReorder: {
+					fixedColumnsLeft: 1
+				},
+				scrollX: true,
+				fixedColumns: {
+					leftColumns: 1
+				},
 				language: {
 					search: '_INPUT_',
 					searchPlaceholder: 'Search records',
-				}
-
-			});
-
-			selector[0].addEventListener('click', function(event) {
-				let target = event.target;
-
-				if (target) {
-					if (target.classList.contains(settings.editClass)) {
-						event.preventDefault();
-
-            let row = (target.closest('tr').id === '') ? target.closest('tr').previousElementSibling : target.closest('tr');
-						editor.edit(target.closest('tr'), {
-				            title: 'Edit record',
-				            buttons: 'Update'
-				        });
-					}else if (target.classList.contains(settings.deleteClass)) {
-						event.preventDefault();
-
-						let row = (target.closest('tr').id === '') ? target.closest('tr').previousElementSibling : target.closest('tr');
-						editor.remove(row, {
-				            title: 'Delete record',
-				            message: 'Are you sure you wish to remove this record?',
-				            buttons: 'Delete'
-				        });
-					}else if (target.classList.contains(settings.addClass)) {
-						event.preventDefault();
-
-						editor.create( {
-				            title: 'Create new record',
-				            buttons: 'Add'
-				        });
-					}
+				},
+				buttons: {
+					dom: {
+						button: {
+							tag: 'button',
+							className: 'btn btn-sm'
+						},
+						container: {
+							className: 'record-editing-buttons'
+						}
+					},
+					buttons: [
+						{ extend: 'create', editor: editor, className: 'btn-default', text:'<i class="fal fa-plus"></i> Add'},
+						{ extend: 'edit',   editor: editor, className: 'btn-default', text:'<i class="fal fa-pencil"></i> Edit'},
+						{ extend: 'remove', editor: editor, className: 'btn-danger', text:'<i class="fal fa-times"></i> Delete'}
+					]
+				},
+				initComplete: function(tSettings, json) {
+					table.buttons().container().appendTo(container.find(settings.buttonContainer),table.table().container());
 				}
 			});
+
+			// container[0].addEventListener('click', function(event) {
+			// 	let target = event.target;
+
+			// 	if (target) {
+			// 		if (target.classList.contains(settings.editClass)) {
+			// 			event.preventDefault();
+
+			// 			let row = (target.closest('tr').id === '') ? target.closest('tr').previousElementSibling : target.closest('tr');
+			// 			editor.edit(row, {
+			// 				title: 'Edit record',
+			// 				buttons: 'Update'
+			// 			});
+			// 		}else if (target.classList.contains(settings.deleteClass)) {
+			// 			event.preventDefault();
+
+			// 			let row = (target.closest('tr').id === '') ? target.closest('tr').previousElementSibling : target.closest('tr');
+			// 			editor.remove(row, {
+			// 				title: 'Delete record',
+			// 				message: 'Are you sure you wish to remove this record?',
+			// 				buttons: 'Delete'
+			// 			});
+			// 		}else if (target.classList.contains(settings.addClass)) {
+			// 			event.preventDefault();
+
+			// 			editor.create( {
+			// 				title: 'Create new record',
+			// 				buttons: 'Add'
+			// 			});
+			// 		}
+			// 	}
+			// });
 		});
 	}
 };
